030265f8ae26c4408a4bd186dae2db8ccb31bc5c
wesbourke <wesbourkeavl@gmail.com>
2023-02-10 17:40:28 -0500

    Project started in Java springboot. Uses spring web package, maven for CDI, and Tomcat as the server. So far project compiles and server runs.

---
+++ src/main/java/com/in28minutes/springboot/learnSpringBoot/CourseController.java
@@ -0,0 +1,20 @@
+package com.in28minutes.springboot.learnSpringBoot;
+import com.in28minutes.springboot.learnSpringBoot.Course;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @RequestMapping("/course")
+    public List<Course> retrieveAllCourses() {
+        return Arrays.asList(
+                new Course(1, "Learn AWS", "in28Minutes"),
+                new Course(2, "Learn DevOps", "in28Minutes")
+        );
+    }
+};
